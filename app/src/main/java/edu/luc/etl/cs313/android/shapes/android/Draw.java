740cf0a9ffd9a61f353b338a3c0a8f610f7885cb
mcrank1 <mcrank@luc.edu>
2024-07-07 15:23:43 -0500

    fixed lines 20 and 21 in the draw.java class

--- app/src/main/java/edu/luc/etl/cs313/android/shapes/android/Draw.java
+++ app/src/main/java/edu/luc/etl/cs313/android/shapes/android/Draw.java
@@ -17,8 +17,8 @@ public class Draw implements Visitor<Void> {
     private final Paint paint;
 
     public Draw(final Canvas canvas, final Paint paint) {
-        this.canvas = null; // FIXME
-        this.paint = null; // FIXME
+        this.canvas = canvas; // FIXME
+        this.paint = paint; // FIXME
         paint.setStyle(Style.STROKE);
     }
 
