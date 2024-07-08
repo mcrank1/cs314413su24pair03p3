1e30c9d6b4a55838f4532b19fadf41451e45dfe7
mcrank1 <mcrank@luc.edu>
2024-07-08 12:32:29 -0500

    Fixed onGroup method to iterate over each shape in the group, used min max values to track coordinates, and return a new bounding box that contains all shapes in the group

--- app/src/main/java/edu/luc/etl/cs313/android/shapes/model/BoundingBox.java
+++ app/src/main/java/edu/luc/etl/cs313/android/shapes/model/BoundingBox.java
@@ -24,8 +24,28 @@ public class BoundingBox implements Visitor<Location> {
 
     @Override
     public Location onGroup(final Group g) {
+        //initialize min and max value for x and y
+        int minX = Integer.MAX_VALUE;
+        int minY = Integer.MAX_VALUE;
+        int maxX = Integer.MIN_VALUE;
+        int maxY = Integer.MIN_VALUE;
 
-        return null;
+        //Iterate over each shape in group to determine bounding box
+        for(Shape shape : g.getShape()) {
+            //calculate the bounding box of the current shape
+            Location loc = shape.accept(this);
+            Rectangle rect = (Rectangle) loc.getShape();
+            int x = loc.getX();
+            int y = loc.getY();
+
+            //update min and max values based onn the current bounding box
+            minX = Math.min(minX, x);
+            minY = Math.min(minY, y);
+            maxX = Math.max(maxX, x + rect.getWidth());
+            maxY = Math.max(maxY, y + rect.getWidth());
+        }
+        //return the bounding box that contains all the shapes in the group
+        return new Location(minX, minY, new Rectangle(maxX - minX, maxY - minY));
     }
 
     @Override
