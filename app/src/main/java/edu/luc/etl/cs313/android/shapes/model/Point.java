b7c6c7d480d6e25184101f4cc86c0d0888b70d86
Manali Deb <Debook4@gmail.com>
2024-07-13 02:29:56 -0500

    fixes to TODO

--- app/src/main/java/edu/luc/etl/cs313/android/shapes/model/Point.java
+++ app/src/main/java/edu/luc/etl/cs313/android/shapes/model/Point.java
@@ -5,9 +5,6 @@ package edu.luc.etl.cs313.android.shapes.model;
  */
 public class Point extends Location {
 
-    // TODO your job
-    // HINT: use a circle with radius 0 as the shape!
-
     public Point(final int x, final int y) {
         super(x, y, new Circle(0));
         assert x >= 0;
