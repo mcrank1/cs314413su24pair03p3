2b0f2b015be6792ef2fd729fcedb6600b60baeb5
Manali Deb <Debook4@gmail.com>
2024-07-13 02:01:18 -0500

    fixed point class

--- app/src/main/java/edu/luc/etl/cs313/android/shapes/model/Point.java
+++ app/src/main/java/edu/luc/etl/cs313/android/shapes/model/Point.java
@@ -9,7 +9,7 @@ public class Point extends Location {
     // HINT: use a circle with radius 0 as the shape!
 
     public Point(final int x, final int y) {
-        super(-1, -1, null);
+        super(x, y, new Circle(0));
         assert x >= 0;
         assert y >= 0;
     }
